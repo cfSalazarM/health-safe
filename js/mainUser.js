d90e7c41353db5854a353fe5d756dee1e5fd109e
YeiMicolta <ymicolta@udenar.edu.co>
2023-02-11 12:25:06 -0500

    Fron-end AdminHome

---
+++ js/mainUser.js
@@ -0,0 +1,27 @@
+document.write(
+    '<nav class="navbar navbar-expand-lg bg-body-tertiary">' +
+    '<div class="container-fluid">' +
+    '<a class="navbar-brand" href="#"><img src="../assets/logo.png" width="120px" height="100px" alt=""></a>' +
+    '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"' +
+    'aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">' +
+    '<span class="navbar-toggler-icon"></span>' +
+    '</button>' +
+    '<div class="collapse navbar-collapse" id="navbarNavAltMarkup">' +
+    '<div class="navbar-nav">' +
+    '<a class="nav-link active" aria-current="page" href="#">Home</a>' +
+    '<a class="nav-link" href="#">Mi Perfil</a>' +
+    '<a class="nav-link" href="#">Admin Medicamentos</a>' +
+    '</div>' +
+    '</div>' +
+    '<div class="row align-items-center">' +
+    '<div class="col">' +
+    '<a class="btn btn-primary" role="button" href=" ">Cerrar sesión</a>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</nav>' +
+    '<div class="align-items-center">' +
+    '<img class="position-absolute top-50 start-50 translate-middle" width="800" height="800"' +
+    'src="../assets/logo.png" alt="">' +
+    '</div>'
+)
