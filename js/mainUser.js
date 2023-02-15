5529e3dea5d9d5ae2c755ada41f9244d01904eee
AngieSegura <angieyurley06@gmail.com>
2023-02-15 11:10:39 -0500

    interfaz de usuraio terminada

--- js/mainUser.js
+++ js/mainUser.js
@@ -1,27 +1,23 @@
 document.write(
     '<nav class="navbar navbar-expand-lg bg-body-tertiary">' +
     '<div class="container-fluid">' +
-    '<a class="navbar-brand" href="#"><img src="../assets/logo.png" width="120px" height="100px" alt=""></a>' +
+    '<a class="navbar-brand" href="../html/UserHome.html"><img src="../assets/logo.png" width="120px" height="100px" alt=""></a>' +
     '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"' +
     'aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">' +
     '<span class="navbar-toggler-icon"></span>' +
     '</button>' +
     '<div class="collapse navbar-collapse" id="navbarNavAltMarkup">' +
     '<div class="navbar-nav">' +
-    '<a class="nav-link active" aria-current="page" href="">Inicio</a>' +
-    '<a class="nav-link" href="#">Mi Perfil</a>' +
-    '<a class="nav-link" href="#">Admin Medicamentos</a>' +
+    '<a class="nav-link active" aria-current="page" href="../html/UserHome.html">Inicio</a>' +
+    '<a class="nav-link" href="../html/UserProfile.html">Mi Perfil</a>' +
+    '<a class="nav-link" href="../html/UserMedicamentos.html">Admin Medicamentos</a>' +
     '</div>' +
     '</div>' +
     '<div class="row align-items-center">' +
     '<div class="col">' +
-    '<a class="btn btn-primary" role="button" href=" ">Cerrar sesión</a>' +
+    '<a class="btn btn-primary" role="button" href="../html">Cerrar sesión</a>' +
     '</div>' +
     '</div>' +
     '</div>' +
-    '</nav>' +
-    '<div class="align-items-center">' +
-    '<img class="position-absolute top-50 start-50 translate-middle" width="800" height="800"' +
-    'src="../assets/logo.png" alt="">' +
-    '</div>'
+    '</nav>'
 )
